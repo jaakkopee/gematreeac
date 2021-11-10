21d7607c80410d695fc5a8e1df3664347000c7e3
Jaakko Prättälä <iipekaj@gmail.com>
2021-11-10 22:23:49 +0200

    Update gematree_tree.py

--- gematree_tree.py
+++ gematree_tree.py
@@ -5,14 +5,36 @@ from gematree_funcs import *
 def getPath(word):
     return listToInt(parentList(getGematria(word)))
 
-wordsnpaths = []
+class Word:
+    def __init__(self, word):
+        self.value = getGematria(word)
+        self.word = word
+        self.path = getPath(word)
+        return
+
+    def printSelf(self):
+        print(self.word)
+        for i in self.path:
+            print(i)
+        return
+
+words = []
 
 def addWord(word):
-    global wordsnpaths
-    path = getPath(word)
-    wordsnpaths += [[word, path]]
-
-def printTree():
-    print(wordsnpaths)
+    global words
+    words += [Word(word)]
     return
 
+def printAll():
+    global words
+    for i in words:
+        print(i.word+" "+str(i.value))
+        print (i.path)
+    return
+
+def printSelves():
+    global words
+    for i in words:
+        i.printSelf()
+    return
+    
