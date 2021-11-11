aa960e7fd974b5136239c8ffa93c9743004c5b81
Jaakko Prättälä <iipekaj@gmail.com>
2021-11-11 06:21:11 +0200

    testing gematree_tree.py

--- gematree_tree.py
+++ gematree_tree.py
@@ -18,18 +18,14 @@ class Word:
             print(i)
         return
 
+
+
 words = []
+
 
 def addWord(word):
     global words
     words += [Word(word)]
-    return
-
-def printAll():
-    global words
-    for i in words:
-        print(i.word+" "+str(i.value))
-        print (i.path)
     return
 
 def printSelves():
@@ -37,4 +33,3 @@ def printSelves():
     for i in words:
         i.printSelf()
     return
-    
