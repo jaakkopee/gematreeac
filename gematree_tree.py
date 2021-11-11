05b7bed35fb6fcf0348653eeaba294e181c02671
Jaakko Prättälä <iipekaj@gmail.com>
2021-11-11 22:58:50 +0200

    Update gematree_tree.py

--- gematree_tree.py
+++ gematree_tree.py
@@ -2,18 +2,38 @@ from gematree_funcs import *
 
 #PROJECT STATUS: broken, I'm thinking..
 
-def getPath(number):
-    return listToInt(parentList(number))
-
 class NumberNode:
     def __init__(self, number):
+        global paths
         self.parent = None
-        self.children = []
         self.value = number
         if len(str(number))>1:
-            self.parent = NumberNode(getPath(number)[len(str(number))-1])
+            self.parent = NumberNode(findParent(number))
 
     def printSelf(self):
         print (" "*len(str(self.value))+str(self.value))
         if self.parent:
             self.parent.printSelf()
+
+
+words = []
+paths = []
+
+def addWord(word):
+    global words, paths
+    gemValue = getGematria(word)
+    words += [(word, gemValue)]
+    inPaths = False
+    for i in paths:
+        if i.value == gemValue:
+            inPaths = True
+    if not inPaths:
+        paths += [NumberNode(gemValue)]
+
+def printAll():
+    for i in paths:
+        i.printSelf()
+        for j in words:
+            if j[1] == i.value:
+                print (j[0])
+    
