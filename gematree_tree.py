7b51193d9e2f35610e6f71b2d3e89ade1a36edaf
Jaakko Prättälä <iipekaj@gmail.com>
2021-11-12 07:27:18 +0200

    Update gematree_tree.py

--- gematree_tree.py
+++ gematree_tree.py
@@ -4,7 +4,7 @@ from gematree_funcs import *
 
 #TODO: sort paths by 1-digit numbered nodes;
 #do something about combining paths, no duplicates.
-#check and save permutations of numbers too (this affects class NumberNode)
+#check and save permutations of numbers too. this affects class NumberNode,
 #it needs to be able to hold multiple numbers
 
 words = []
