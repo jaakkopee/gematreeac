8d49aca8f40377f63967da50172f08d60e32f2b7
Jaakko Prättälä <iipekaj@gmail.com>
2021-11-18 22:20:58 +0200

    a rewritten algorithm included
    new files: gemadbconn.py gematriac.db gt2.py, where the last one has the new data representation

---
+++ gemadbconn.py
@@ -0,0 +1,61 @@
+from gt2 import *
+import sqlite3
+con = sqlite3.connect("gematriac.db")
+cur = con.cursor()
+
+cur.execute("create table if not exists wordpool (word text)")
+con.commit()
+
+def getWordsFromSQL():
+    global con, cur
+    cur.execute("select * from wordpool")
+    data = cur.fetchall()
+    return data
+
+
+def insertWord(word):
+    global con, cur
+    
+    WORDMAP.clearRAM() #empty RAM
+
+    #insert new word to SQL DB
+    #TODO: check for word duplicates
+    cur.execute("insert into wordpool values (:wordstr) ", {"wordstr": word})
+    con.commit()
+
+    #build tree
+    wordpool = getWordsFromSQL()
+    for i in wordpool:
+        WORDMAP.addWord(i[0])
+    
+    #print tree
+    WORDMAP.printMapping()
+
+    return
+
+def printDB():
+    global con, cur
+    
+    WORDMAP.clearRAM() #empty RAM
+
+    #load db to RAM
+    wordpool = getWordsFromSQL()
+    
+    #build tree
+    for i in wordpool:
+        WORDMAP.addWord(i[0])
+    
+    #print tree
+    WORDMAP.printMapping()
+
+    return
+
+def emptyDB():
+    global cur,con
+    cur.execute("delete from wordpool")
+    con.commit()
+    return
+
+def closeDB():
+    global con
+    con.close()
